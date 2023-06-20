ac5322452bd2efc9cb56a856f02295b1cacf614d
ezjing <god998877@naver.com>
2023-06-20 18:16:49 +0900

    kotlin_0620(http 통신(retrofit)을 이용하여 뉴스 앱 만들기)

---
+++ Ch18_network/ch18_news/src/main/java/com/busanit/ch18_news/INetworkService.kt
@@ -0,0 +1,14 @@
+package com.busanit.ch18_news
+
+import com.busanit.ch18_news.model.PageListModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.nio.channels.spi.AbstractSelectionKey
+
+interface INetworkService { // 레트로핏을 사용할때 쓰는 인터페이스
+    @GET("/v2/everything")
+    fun getList(
+        @Query("q") q: String?, @Query("apiKey") apiKey: String?, @Query("page") page: Long, @Query("pageSize") pageSize: Int
+    ): Call<PageListModel>  // 쿼리스트링으로 받아올 4가지 항목
+}
