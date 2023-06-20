98a59c63c25fd33cf5ef04a564746ed245c0066a
ezjing <god998877@naver.com>
2023-06-20 18:14:56 +0900

    kotlin_0619(http 통신(volley)을 이용하여 뉴스 앱 만들기)

--- Ch18_network/ch18_news/src/main/java/com/busanit/ch18_news/VolleyFragment.kt
+++ Ch18_network/ch18_news/src/main/java/com/busanit/ch18_news/VolleyFragment.kt
@@ -17,37 +17,37 @@ import com.busanit.ch18_news.model.ItemModel
 import com.google.gson.JsonObject
 import org.json.JSONObject
 
-class VolleyFragment : Fragment() {
+class VolleyFragment : Fragment() { // 현 프래그먼트에선 json 방식 사용
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentVolleyBinding.inflate(inflater, container, false)
         val volleyRecyclerView = binding.volleyRecyclerView
-        // url 정리
+        // url 정리(기본 URL 에 검색어와 key 입력)
         val url = MyApplication.BASE_URL + "/v2/everything?q=${MyApplication.Query}&apiKey=" +
                 "${MyApplication.API_KEY}&page=1&pageSize=10"
-        // RequestQueue 생성
+        // RequestQueue 생성(RequestQueue : 서버에 요청을 보내는 역할, 여기서 activity는 프래그먼트를 말하며 해당 프래그먼트를 서버에 요청 보냄)
         val queue = Volley.newRequestQueue(activity)
 
         val jsonRequest = object : JsonObjectRequest(
-            Request.Method.GET, url, null, Response.Listener<JSONObject> {response ->
-                val jsonArray = response.getJSONArray("articles")   // 반복되는 태그는 array
-                val itemList = mutableListOf<ItemModel>()
-                for (i in 0 until jsonArray.length()) {
+            Request.Method.GET, url, null, Response.Listener<JSONObject> {response ->   // 통신 성공 ㅅ
+                val jsonArray = response.getJSONArray("articles")   // 반복되는 태그는 array(그냥 배열 받아 오는 것)
+                val itemList = mutableListOf<ItemModel>()   // List 받아오기
+                for (i in 0 until jsonArray.length()) { // 배열의 크기만큼 반복
                     ItemModel().run {
-                        val article = jsonArray.getJSONObject(i)    // 하나뿐인 태그는 object
+                        val article = jsonArray.getJSONObject(i)    // 하나뿐인 태그는 object(각 컬럼 가져오기)
                         author = article.getString("author")
                         title = article.getString("title")
                         description = article.getString("description")
                         urlToImage = article.getString("urlToImage")
                         publishedAt = article.getString(("publishedAt"))
-                        itemList.add(this)  // this 는 ItemModel을 말함.
+                        itemList.add(this)  // this 는 ItemModel을 말함.(List에 배열을 추가)
                     }
                 }
-                volleyRecyclerView.layoutManager = LinearLayoutManager(activity)
-                volleyRecyclerView.adapter = MyAdapter(activity as Context, itemList)
-            }, Response.ErrorListener {error ->
+                volleyRecyclerView.layoutManager = LinearLayoutManager(activity)    // 리사이클러뷰 매니저
+                volleyRecyclerView.adapter = MyAdapter(activity as Context, itemList)   // 리사이클러뷰 어댑터
+            }, Response.ErrorListener {error -> // 통신 실패시
                 Log.d("myLog", "통신 실패 : $error")
             }
         ) {
@@ -58,7 +58,7 @@ class VolleyFragment : Fragment() {
                 return map
             }
         }
-        queue.add(jsonRequest)
+        queue.add(jsonRequest)  // queue에 json으로 받아온 데이터 추가
 
         return binding.root
     }
